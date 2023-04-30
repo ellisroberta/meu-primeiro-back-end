a2a896582fe38ade86d841320550d29cea490378
ellisroberta <ellisroberta@hotmail.com>
2023-04-30 18:59:20 -0300

    adiciona a primeira versao do meu projeto

---
+++ mulher.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const router = express.Router()
+
+const app = express()
+const porta = 3333
+
+function mostraMulher(request, response) {
+    response.json({
+        nome: 'Ellis Roberta',
+        imagem: 'https://encurtador.com.br/sETU',
+        minibio: 'Engenheira de materiais e Desenvolvedora backend'
+    })
+}
+
+function mostraPorta() {
+    console.log("Servidor criado e rodando na porta ", porta)
+}
+
+app.use(router.get('/mulher', mostraMulher))
+app.listen(porta, mostraPorta)
